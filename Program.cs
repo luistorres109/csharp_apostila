8a681eb346e9c1ed204e7f46ed46dd1bde29d0e6
Luís Miguel Torres <94331066+luistorres109@users.noreply.github.com>
2025-03-26 16:38:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using BIBLIOTECA_APOSTILA.Data;
+using BIBLIOTECA_APOSTILA.Helper;
+using BIBLIOTECA_APOSTILA.Models;
+using BIBLIOTECA_APOSTILA.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True"));
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+builder.Services.AddSingleton<ISessao, Sessao>();
+
+builder.Services.AddSession(o =>
+{
+    o.Cookie.HttpOnly = true;
+    o.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Login}/{action=Index}/{id?}");
+
+app.Run();
